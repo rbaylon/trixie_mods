85c6c6009cd5ab351cd85506f59f64d51867f889
eking <eking@dev.local>
2024-10-21 18:32:30 +0800

    specific routes

--- auth/routes/routes.go
+++ auth/routes/routes.go
@@ -8,15 +8,15 @@ import (
 func GetRouter() *http.ServeMux {
 	Router := http.NewServeMux() // 3
 
+	Router.HandleFunc("GET /users", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Get all users")
+	})
+
 	Router.HandleFunc("/sign-in", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Customer Sign in")
 	})
 
-	Router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Get all users")
-	})
-
-	Router.HandleFunc("GET /{id}", func(w http.ResponseWriter, r *http.Request) {
+	Router.HandleFunc("GET /users/{id}", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Get user with id: %d", r.PathValue("id"))
 	})
 
